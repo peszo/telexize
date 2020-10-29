31fd735838112ac1a4b4b450601c3d438e960f66
peszo <revkomarom@gmail.com>
2020-10-29 10:34:06 +0100

    removed console log

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.webNavigation.onCommitted.addListener(function() {
-  console.log("this is index!");
   chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
     let newurl = "https://telex.hu";
     chrome.tabs.update(tab.id, {url: newurl});
